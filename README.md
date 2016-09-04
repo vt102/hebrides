bb977c210a6f6a4da701f83b5660fc0b6634eb19
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2016-09-04 19:14:16 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# hebrides
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'hebrides'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures hebrides'
+long_description 'Installs/Configures hebrides'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/hebrides/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/hebrides' if respond_to?(:source_url)
