3b6e813d28fc884babe1d999ddfaed348236f999
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2016-09-06 01:16:10 +0000

    victor, year from databag

--- recipes/website.rb
+++ recipes/website.rb
@@ -6,12 +6,16 @@
 
 require 'time'
 
+app = search(:aws_opsworks_app).first
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   owner  'apache'
   group  'apache'
   mode   '0755'
   variables(
-    now: Time.now.utc.iso8601
+    now: Time.now.utc.iso8601,
+    victor: app['victor'],
+    year: app['year']
   )
 end
