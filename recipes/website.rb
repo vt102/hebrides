bbaf679da75e3a6cad237d436ec8377e4154774b
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2016-09-06 01:19:34 +0000

    victor, year from databag

--- recipes/website.rb
+++ recipes/website.rb
@@ -5,8 +5,10 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 require 'time'
+require 'json'
 
 app = search(:aws_opsworks_app).first
+log(app.to_json)
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
