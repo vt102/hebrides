af7399ab406acb4d8a745e60e3c8186880777792
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2016-09-06 00:27:05 +0000

    added timestamp to website

--- recipes/website.rb
+++ recipes/website.rb
@@ -4,9 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+require 'time'
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   owner  'apache'
   group  'apache'
   mode   '0755'
+  variables {
+    now: Time.now.utc.iso8601
+  }
 end
