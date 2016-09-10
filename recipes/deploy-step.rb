d35c823cd4fa630dfa54fb480a7b7f7499783436
Andy Cowell <Andrew.Cowell@ScrippsNetworks.com>
2016-09-10 14:55:46 +0000

    Moved website to hebrides-payload repo.  Added app and deploy data bag capture.

--- recipes/deploy-step.rb
+++ recipes/deploy-step.rb
@@ -4,9 +4,17 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+require 'json'
 require 'time'
 
 t = Time.now.iso8601
-
 file "/tmp/deploy-#{t}"
+
+File.open('/tmp/app.json', 'w') { |f|
+  f.write(search(:aws_opsworks_app).first.to_json)
+}
+
+File.open('/tmp/deploy.json', 'w') { |f|
+  f.write(search(:aws_opsworks_deploy).first.to_json)
+}
 
